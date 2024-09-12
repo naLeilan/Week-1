78d2dff08ed7b630a5a41247453591b82b9d1193
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-09-12 11:05:24 +0300

    add base elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <nav className="nav">
+        <About />
+        <Projects />
+        <Skills />
+        <ContactUs />
+      </nav>
+      <div className="app">app</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,5 @@
+function ContactUs() {
+  return <div>Contact</div>;
+}
+
+export default ContactUs;
