a6a3066fdb15a5b686e90e14f8b922b978748c70
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-09-13 12:17:43 +0300

    fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { MdDarkMode } from "react-icons/md";
-import { FaGithub, FaL, FaLinkedinIn, FaN, FaTwitter } from "react-icons/fa6";
+import About from "./pages/About";
+import { FaL, FaN } from "react-icons/fa6";
+import Footer from "./components/Footer";
 
 function App() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <div className="app">
       <nav className="nav">
@@ -15,39 +15,14 @@ function App() {
           <a>About</a>
           <a>Projects</a>
           <a>Skills</a>
-          <a>ContactUs</a>
+          <a>Contact Us</a>
         </div>
         <div className="darkmode">
           <MdDarkMode />
         </div>
       </nav>
-      <div className="main">
-        <h2>Full-time Freelancer</h2>
-        <p>
-          Full-time Freelancer is a short, self-published book that offers a
-          concise view of the finances involved in freelancing full-time. It
-          contains real financial numbers from my freelancing experience in 2010
-          and 2011. I wrote it for those who are planning to go out on their own
-          at some point and would like to see a real-world scenario of what that
-          looks like. It’s the book I wish I had been able to read before I
-          dived in to freelancing full-time.
-        </p>
-      </div>
-      <footer className="footer">
-        <FaTwitter className="twitter" />
-        <FaGithub className="github" />
-        <FaLinkedinIn className="linkedin" />
-        <p className="copyright">
-          Copyright &copy;
-          <a
-            className="Github-link"
-            target="_blank"
-            href="https://github.com/NaLeilan"
-          >
-            Leilan Na {currentYear}
-          </a>
-        </p>
-      </footer>
+      <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa6";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <a href="https://x.com/NaLeilan" target="_blank">
+        <FaTwitter className="twitter" />
+      </a>
+      <a href="https://github.com/NaLeilan">
+        <FaGithub className="github" />
+      </a>
+      <a href="">
+        <FaLinkedinIn className="linkedin" />
+      </a>
+
+      <p className="copyright">
+        Copyright &copy;
+        <a
+          className="Github-link"
+          target="_blank"
+          href="https://github.com/NaLeilan"
+        >
+          Leilan Na {currentYear}
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
