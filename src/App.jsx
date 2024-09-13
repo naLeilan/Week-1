3dec38f5e9f4136019b1c3f2f1190ef638bdf600
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-09-13 13:25:00 +0300

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import Logo from "./components/Logo";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import ContactUs from "./pages/ContactUs";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <div className="app">
-      <Logo />
-      <About />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="about" element={<About />} />
+        <Route path="contactus" element={<ContactUs />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <div className="app">
+        <Logo />
+        <About />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -10,10 +10,10 @@ function Logo() {
         <img src="/icon.png" alt="icon" height="70px" />
       </div>
       <div className="navitems">
-        <a>About</a>
+        {/* <a>About</a>
         <a>Projects</a>
         <a>Skills</a>
-        <a>Contact Us</a>
+        <a>Contact Us</a> */}
       </div>
       <div className="darkmode">
         <MdDarkMode />
