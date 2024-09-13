f418f0eb6ed1f5c5377adb2e4ac133ac1686ddb5
naleilan <7776224+naLeilan@users.noreply.github.com>
2024-09-13 11:55:04 +0300

    add main

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,18 @@ function App() {
           <MdDarkMode />
         </div>
       </nav>
-      <div className="main">app</div>
+      <div className="main">
+        <h2>Full-time Freelancer</h2>
+        <p>
+          Full-time Freelancer is a short, self-published book that offers a
+          concise view of the finances involved in freelancing full-time. It
+          contains real financial numbers from my freelancing experience in 2010
+          and 2011. I wrote it for those who are planning to go out on their own
+          at some point and would like to see a real-world scenario of what that
+          looks like. It’s the book I wish I had been able to read before I
+          dived in to freelancing full-time.
+        </p>
+      </div>
       <footer className="footer">
         <FaTwitter className="twitter" />
         <FaGithub className="github" />
